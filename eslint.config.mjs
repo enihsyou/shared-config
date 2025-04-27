ddb4ba2af65bf6562dc41cd216d5366364ef4b70
九条涼果 <enihsyou@gmail.com>
2025-04-27 14:55:15 +0800

    bump packages/editor-config to 0.0.2

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from "eslint/config";
 import eslintConfig from "@enihsyou/eslint-config";
 
 export default defineConfig([
-	{
-		files: ["**/*.mjs"],
-		extends: [eslintConfig],
-	},
+    {
+        files: ["**/*.mjs"],
+        extends: [eslintConfig],
+    },
 ]);
