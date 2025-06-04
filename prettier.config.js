2c6299cba67d2b3047564228a2b9d4de012cd45f
九条涼果 <enihsyou@gmail.com>
2025-06-04 15:24:20 +0800

    切换为使用 EditorConfig 版本的 Prettier Config

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+import { withEditorConfig as enihsyou } from "@enihsyou/prettier-config";
+
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+    ...enihsyou,
+};
+
+export default config;
