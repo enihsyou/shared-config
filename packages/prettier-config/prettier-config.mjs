c90dc94faab26fe9e84482b7d3acd8880af328bb
九条涼果 <enihsyou@gmail.com>
2025-12-06 15:10:33 +0800

    只保留非默认值

--- packages/prettier-config/prettier-config.mjs
+++ packages/prettier-config/prettier-config.mjs
@@ -11,34 +11,8 @@
 const config = {
   // 在 4 空格缩进上宽度要适当增加
   printWidth: 120,
-  // 适当留空更美观
-  // tabWidth: 4, // 使用 Prettier 默认值
-  // （Prettier默认）当然不使用 Tab
-  useTabs: false,
-  // （Prettier默认）反正分号是自动添加的不费事
-  semi: true,
-  // （Prettier默认）双引号看习惯了，输入时允许少按 shift
-  singleQuote: false,
-  // （Prettier默认）属性一行放多个更充分利用空间
-  singleAttributePerLine: false,
-  // （Prettier默认）允许尾随逗号方便修改是好文明
-  trailingComma: "all",
-  // （Prettier默认）行内对象前后留空格更美观
-  bracketSpacing: true,
-  // （Prettier默认）半人工控制该怎么样就怎么样
-  objectWrap: "preserve",
-  // （Prettier默认）半人工控制该怎么样就怎么样
-  proseWrap: "preserve",
-  // （Prettier默认）和尾随逗号一样方便修改
-  bracketSameLine: false,
-  // （Prettier默认）有括号方便展示 Inlay Hints
-  arrowParens: "always",
-  // （Prettier默认）有必要时再写出来
-  quoteProps: "as-needed",
   // 操作符换行后放在前面更美观
   experimentalOperatorPosition: "start",
-  // （Prettier默认）不喜欢新样式，?:对齐多好看
-  experimentalTernaries: false,
   // 文件特殊规则
   overrides: [
     {
